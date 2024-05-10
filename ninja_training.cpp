1e4a778b58a1817210835ef74de163b4d509750c
Pranay Agrawal <100509183+Pranay38@users.noreply.github.com>
2024-05-10 14:25:42 +0530

    space optimization

--- ninja_training.cpp
+++ ninja_training.cpp
@@ -35,26 +35,56 @@ using namespace std;
 // }
 
 //-----------TABULATION------------------
+// int f(int n,vector<vector<int>>&points){
+//     vector<vector<int>>dp(n,vector<int>(4,0));
+//     dp[0][0]=fmax(points[0][1],points[0][2]);
+//     dp[0][1]=fmax(points[0][0],points[0][2]);
+//     dp[0][2]=fmax(points[0][0],points[0][1]);
+//     dp[0][3]=fmax(points[0][0],fmax(points[0][1],points[0][2]));
+
+//     for(int day=1;day<n;day++){
+//         for(int last=0;last<=3;last++){
+//             dp[day][last]=0;
+//             for(int task=0;task<=2;task++){
+//                 if(task!=last){
+//                     int activity=points[day][task]+dp[day-1][task];
+//                     dp[day][last]=fmax(dp[day][last],activity);
+
+//                 }
+//             }
+//         }
+//     }
+//     return dp[n-1][3];
+// }
+// int main(){
+//     vector<vector<int>> points = {{10, 40, 70},{20, 50, 80},{30, 60, 90}};
+//     int n=points.size();
+    
+//     cout<<f(n,points);
+// }
+
+
+//---------Space Optimization--------------
 int f(int n,vector<vector<int>>&points){
-    vector<vector<int>>dp(n,vector<int>(4,0));
-    dp[0][0]=fmax(points[0][1],points[0][2]);
-    dp[0][1]=fmax(points[0][0],points[0][2]);
-    dp[0][2]=fmax(points[0][0],points[0][1]);
-    dp[0][3]=fmax(points[0][0],fmax(points[0][1],points[0][2]));
-
+  vector<int>prev(4,0);
+    prev[0]=fmax(points[0][1],points[0][2]);
+    prev[1]=fmax(points[0][0],points[0][2]);
+    prev[2]=fmax(points[0][0],points[0][1]);
+    prev[3]=fmax(points[0][0],fmax(points[0][1],points[0][2]));
     for(int day=1;day<n;day++){
-        for(int last=0;last<=3;last++){
-            dp[day][last]=0;
-            for(int task=0;task<=2;task++){
+        vector<int>temp(4,0);
+        for(int last=0;last<4;last++){
+            temp[last]=0;
+            for(int task=0;task<3;task++){
                 if(task!=last){
-                    int activity=points[day][task]+dp[day-1][task];
-                    dp[day][last]=fmax(dp[day][last],activity);
-
+                     temp[last]=fmax(temp[last],points[day][task]+prev[task]);
                 }
-            }
         }
     }
-    return dp[n-1][3];
+    prev=temp;
+}
+return prev[3];
+
 }
 int main(){
     vector<vector<int>> points = {{10, 40, 70},{20, 50, 80},{30, 60, 90}};
